ae58a62365c4e45840a31af86bced2a22939611d
reaper9ui <159961080+reaper9ui@users.noreply.github.com>
2024-05-26 11:56:08 -0500

    created dto and map (Last repo was just setting up mySQL & data fields)

---
+++ ems-backend/src/main/java/com/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.ems_backend.mapper;
+
+import com.ems_backend.dto.EmployeeDto;
+import com.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getEmail(),
+                employee.getLastName()
+            );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
